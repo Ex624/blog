613e817d3767dd83af428bbf888200bfb9058ca5
tolgaand <tolgaand@yandex.com>
2020-11-23 20:40:59 +0300

    test

---
+++ public/admin/js/blog.js
@@ -0,0 +1,67 @@
+$(() => {
+  let panelID = 0;
+  let data = null;
+
+  $(document).ajaxStart(() => {
+    NProgress.start();
+    console.log();
+  });
+  $(document).ajaxComplete(() => {
+    NProgress.done();
+  });
+
+  const editorInstance = new FroalaEditor("#edit", {
+    enter: FroalaEditor.ENTER_P,
+    events: {
+      initialized: function () {
+        const editor = this;
+        document.getElementById("preview").innerHTML = editor.html.get();
+      },
+      contentChanged: function () {
+        const editor = this;
+        document.getElementById("preview").innerHTML = editor.html.get();
+      },
+    },
+  });
+
+  $(".next-btn").click(() => {
+    let form = $(`form#form-step-${panelID}`);
+    if (panelID == 0) {
+      data = form.serializeArray().reduce(function (obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+      }, {});
+      data.tags = data.tags.split(",");
+    } else if (panelID == 1) {
+      data.content = $("#preview").html();
+    }
+
+    panelID++;
+    console.log(data);
+    console.log(panelID);
+  });
+  $(".prev-btn").click(() => panelID--);
+  $(".finish-btn").click(() => {
+    if (!(data.title || data.shortContent || data.content || data.category))
+      return;
+    panelID = 2;
+    $.ajax({
+      url: "/admin/blog/add",
+      contentType: "application/json",
+      method: "POST",
+      data: JSON.stringify(data),
+    }).done((r) => {
+      console.log(r);
+      if (r.success) {
+        iziToast.success({
+          title: "Başarılı",
+          message: r.message,
+        });
+      } else
+        iziToast.error({
+          title: "Başarısız",
+          message: r.message,
+        });
+    });
+  });
+});
